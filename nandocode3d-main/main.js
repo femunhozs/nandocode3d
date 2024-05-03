55b0e1f161dc25ef5c11cbcc67219f594d27e97a
Fernando Munhoz <110676069+femunhozs@users.noreply.github.com>
2024-05-03 09:04:07 -0300

    Add files via upload

--- nandocode3d-main/main.js
+++ nandocode3d-main/main.js
@@ -1,7 +1,11 @@
+
 const botoes = document.querySelectorAll(".botao");
-console.log(botoes);
-for (let i=0; i <botoes.length;i++){
+
+for (let i=0; i<botoes.length;i++){
     botoes[i].onclick=function(){
-        botoes[i].classList.add("Ativo");
+        for(j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+        }
+        botoes[i].classList.add("ativo");
     }
 }
